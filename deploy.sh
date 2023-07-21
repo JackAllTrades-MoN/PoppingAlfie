b758eac2563d909eebce9c42c4d73a23dfdd37b5
Jack <magikku009@hotmail.co.jp>
2023-07-21 15:23:54 +0900

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eu
+
+echo "** deploy starts\n"
+git branch -D gh-pages || true
+git checkout -b gh-pages
+cp -rf ./static/* ./
+echo "** add changes\n"
+git add ./* || true
+git commit -m "deploy"
+echo "** push to gh-pages\n"
+git push --force deploy gh-pages
+git checkout master
+echo "** deploy succeeded\n"
